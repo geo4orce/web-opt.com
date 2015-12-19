7e8cf251a1d20c2f3e3c338865f4f99e330b9d75
Geo <geo.artemenko@gmail.com>
2015-12-19 17:10:52 -0500

    update blade to display logo (use asset() helper)

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,55 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Web-Opt</title>
+@extends('master')
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('title')
+    Welcome {{ $title_divider }} @parent
+@endsection
 
+@section('head')
     <style>
-        html, body {
-            height: 100%;
-        }
-
         body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 96px;
+            background-color: #000032;
+            background-image: url('{{ asset('img/logo.png') }}');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center;
         }
     </style>
-</head>
-<body>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-10189458-10', 'auto');
-        ga('send', 'pageview');
-    </script>
-
-    <div class="container">
-        <div class="content">
-            <div class="title">Web-Opt</div>
-        </div>
-    </div>
-</body>
-</html>
+@endsection
