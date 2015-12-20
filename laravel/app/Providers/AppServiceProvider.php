a8a6aafd14bd132191a713babedb87769362c801
Geo <geo.artemenko@gmail.com>
2015-12-20 18:03:40 -0500

    comment

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -13,6 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // variables available to all blades
         view()->share('title_divider', '|');
     }
 
