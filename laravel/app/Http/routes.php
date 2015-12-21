571bf2f574aaf4e793eddd6f1b7077e7f6734dae
Geo <geo.artemenko@gmail.com>
2015-12-20 21:14:57 -0500

    test PHP7 comment

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -4,4 +4,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Test PHP7
+// Route::get('/test/php7', function () {
+//     return (int) (1 <=> 2);
+// });
+
 /* End of file */
