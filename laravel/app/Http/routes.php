be155d057a2d4340abfd95f0faf149ad8ccc87a4
Geo <geo.artemenko@gmail.com>
2016-04-16 15:37:23 -0400

    versions

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -5,7 +5,24 @@ Route::get('/', function () {
 });
 
 Route::get('test', function () {
-    return '<h1>Test OK!</h1>';
+    $lrv = app();
+    $lrv = $lrv::VERSION;
+
+    $php = phpversion();
+
+    $sql = mysqli_get_client_version();
+
+    $out = <<<OUT
+
+    <h1>Test OK!</h1>
+	Built with:
+	<ul>
+		<li>Laravel version $lrv
+		<li>PHP version $php
+		<li>MySql version $sql
+
+OUT;
+	return $out;
 });
 
 // Test PHP7
