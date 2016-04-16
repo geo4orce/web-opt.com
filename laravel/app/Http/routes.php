aa5aead7daa9968ea9852ad3d21d2e927ccd88a4
Geo <geo.artemenko@gmail.com>
2016-04-16 16:16:37 -0400

    test routes

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -4,13 +4,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('phpinfo', function () {
+	return phpinfo();
+});
+
 Route::get('test', function () {
     $lrv = app();
     $lrv = $lrv::VERSION;
 
     $php = phpversion();
 
-    $sql = mysqli_get_client_version();
+    if (function_exists('mysqli_get_client_version')) {
+	    $sql = mysqli_get_client_version();
+    } else {
+    	$sql = '[hidden]';
+    }
 
     $out = <<<OUT
 
@@ -26,8 +34,9 @@ OUT;
 });
 
 // Test PHP7
-// Route::get('/test/php7', function () {
-//     return (int) (1 <=> 2);
-// });
+Route::get('php7', function () {
+    return 'PHP 7';
+    // return (int) (1 <=> 2);
+});
 
 /* End of file */
