3c83a20879aa3e4e65bab0509604449d97b49e47
Geo <geo.artemenko@gmail.com>
2016-04-16 14:59:14 -0400

    test routes

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -4,6 +4,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('test', function () {
+    return '<h1>Test OK!</h1>';
+});
+
 // Test PHP7
 // Route::get('/test/php7', function () {
 //     return (int) (1 <=> 2);
