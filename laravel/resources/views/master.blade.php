a201b1016cb0a56200ea730420cc7b7a9877e9bd
Geo <geo.artemenko@gmail.com>
2016-04-17 19:28:33 -0400

    version 1.0

--- laravel/resources/views/master.blade.php
+++ laravel/resources/views/master.blade.php
@@ -1,24 +1,82 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <title> @section('title') {{ env('APP_NAME') }} @show </title>
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet" type="text/css">
-    <script src="{{ asset('js/main.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('js/google-anal.js') }}" type="text/javascript"></script>
-    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
+    <meta charset="utf-8">
+    <title>W E B &amp; O P T</title>
+    <meta name="description" content="Website Development and Optimization">
+    <meta property="og:title" content="W E B &amp; O P T">
+    <meta property="og:description" content="Website Development and Optimization">
+    <meta name="twitter:card" content="Website Development and Optimization">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="generator" content="SublimeText3">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/webflow.css">
+    <link rel="stylesheet" type="text/css" href="css/web-opt.webflow.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+    <script>
+    WebFont.load({
+      google: {
+        families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Montserrat:400,700"]
+      }
+    });
+    </script>
+    <script type="text/javascript" src="js/modernizr.js"></script>
 </head>
 <body>
-    <header>
-        <h1>{{ env('APP_NAME') }}</h1>
-        @include('shared.nav')
-    </header>
-    <div class="main">
-        @yield('body')
+  <div id="section-home" class="w-section section-header">
+    <div class="w-row">
+      <div class="w-col w-col-6"></div>
+      <div class="w-col w-col-6 w-clearfix">
+        <div class="top-navigation">
+            <a href="#section-home" class="top-navigation-link">Home</a>
+            <a href="#section-works" class="top-navigation-link">Work</a>
+            <a href="#section-footer" class="top-navigation-link">Contacts</a>
+        </div>
+      </div>
     </div>
-    <footer>
-        @include('shared.nav')
-        <div class="legal">Web-Opt &copy; 2011-{{ date('Y') }}</div>
-    </footer>
+    <div class="w-container">
+      <div class="intro-holder">
+        <h1 class="intro-title">Web&amp;Opt</h1>
+        <div class="intro-description">Website Development and Optimization</div>
+      </div>
+    </div>
+  </div>
+  <div id="section-works" class="w-section section-works">
+    <div class="work weareglow">
+      <div class="weareglow-holder">
+        <h1>Glow</h1>
+        <p class="work-description">Award-winning, digital marketing and social media agency based in New York City. Since 1999, GLOW has partnered with high-profile brands and networks to build world-class social and digital campaigns.</p>
+        <a href="http://weareglow.com" target="_blank" class="work-link">www.weareglow.com</a>
+      </div>
+    </div>
+    <div class="work hooklogick">
+      <div class="weareglow-holder">
+        <h1>HookLogic</h1>
+        <p class="work-description">Pioneer of performance marketing for brands, manufacturers and hotels. The company was founded in 2004 and is based in New York, with additional offices in California, United Kingdom and France.</p>
+        <a href="http://hooklogic.com" target="_blank" class="work-link white">www.hooklogic.com</a>
+      </div>
+    </div>
+  </div>
+  <div id="section-footer" class="w-section section-footer">
+    <div class="copyrights">&copy; 2011-{{ date('Y') }} Web-Opt, LLC. All Rights Reserved.</div>
+    <div class="weareglow-holder">
+      <h1>Contacts</h1>
+      <div class="personal-contact">
+        <div class="contact-name">Geo Artemenko | New York</div>
+        <div class="contact-phone">+1 (929) 400-5006</div>
+        <div><a class="contact-email" href="mailto:geo.artemenko@gmail.com">geo.artemenko@gmail.com</a>
+        </div>
+      </div>
+      <div class="personal-contact">
+        <div class="contact-name">Kirill Artemenko | Moscow</div>
+        <div class="contact-phone">+7 (919) 993-9240</div>
+        <div><a class="contact-email" href="mailto:kirill@artemenko.info">kirill@artemenko.info</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <script type="text/javascript" src="js/webflow.js"></script>
+  <!--[if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif]-->
 </body>
 </html>
