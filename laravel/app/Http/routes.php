fbc4a545eba5a5b3d3b9f7737886d98bd55a21a8
Geo <geo.artemenko@gmail.com>
2016-04-16 21:20:40 -0400

    tests

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -4,7 +4,14 @@ Route::get('/',       'Main@home');
 Route::get('clients', 'Main@clients');
 Route::get('contact', 'Main@contact');
 
-Route::get('test',    'Test@test');
+// Tests!!!!
+Route::get('test', function() {
+    if (request()->is('test')) {
+        echo 'yes';
+    } else {
+        echo 'no';
+    }
+});
 Route::get('phpinfo', 'Test@phpinfo');
 Route::get('php7',    'Test@php7');
 
