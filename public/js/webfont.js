78ef7611f89bf66bc6175fd30c5d99e7c84327a0
Geo Artemenko <geo.artemenko@gmail.com>
2018-06-02 19:57:17 -0400

    no inspection

--- public/js/webfont.js
+++ public/js/webfont.js
@@ -1,3 +1,4 @@
+// noinspection JSUnresolvedVariable
 WebFont.load({
   google: {
     families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Montserrat:400,700"]
