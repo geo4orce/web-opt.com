69e5c8c94a055018f0f16edb838071bb214d0429
Geo Artemenko <geo.artemenko@gmail.com>
2018-10-14 18:55:20 -0400

    Upgrade to Laravel 5.7

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/head.js', 'public/build/js')
-    .js('resources/assets/js/foot.js', 'public/build/js')
-    .sass('resources/assets/sass/app.scss', 'public/build/css')
+mix.js('resources/js/head.js', 'public/build/js')
+    .js('resources/js/foot.js', 'public/build/js')
+    .sass('resources/sass/app.scss', 'public/build/css')
     .version()
 ;
