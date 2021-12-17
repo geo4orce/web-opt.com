5df7bf04731a4f40eca06ebf1265be6cd8b10a6f
Geo Artemenko <geo.artemenko@gmail.com>
2021-12-17 00:51:40 -0500

    make gray color darker

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -12,7 +12,7 @@
                 margin: 0;
                 padding: 0;
                 width: 100%;
-                color: #B0BEC5;
+                color: gray;
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato', serif;
