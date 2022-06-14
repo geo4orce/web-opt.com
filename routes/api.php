c207183a002429d788d9f0b2cac855f99b5e5ddc
Geo Artemenko <geo@web-opt.com>
2022-06-14 10:18:53 -0400

    must be not empty

--- routes/api.php
+++ routes/api.php
@@ -6,22 +6,21 @@ use Illuminate\Support\Facades\Mail;
 
 Route::post('contact-us', function (Request $request) {
     $data = $request->all();
-    $from = $request->get('email');
-    $msg = $request->get('message');
+    $from = trim($request->get('email'));
+    $msg = trim($request->get('message'));
     $isBot = $request->get('name'); // this is an invisible honeypot input field, humans will not fill it
     $isBot = $isBot ? ' (bot)' : '';
 
     info('incoming contact-us' . $isBot, $data);
 
     $willSend = !$isBot && $from && $msg; // must be not empty
-
     if ($willSend) {
         Mail::to(config('mail.contact'))
             ->send(new ContactForm($data));
     }
 
     return [
-        'status' => 'OK',
         'message' => "received from '$from'",
+        'status' => 'OK',
     ];
 });
