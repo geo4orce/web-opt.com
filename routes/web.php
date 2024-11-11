aa1f58cc72ceb42951ebd87c7961f9dda50d5897
Geo <geo@web-opt.com>
2024-11-11 17:50:16 -0500

    add qr1 code

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,9 @@
 Route::get('/', 'MasterController@index')
     ->middleware('cache.headers:public;max_age=3600;etag')
     ->name('home');
+
+Route::get('/qr1', function() {
+    Log::info('qr redirect to blinq');
+
+    return Redirect::to('https://blinq.me/f5TLMRTbpG0j', 301);
+});
